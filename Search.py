1e1dd1bf7c6aeef42afa5894266e292f4a0a2d94
codes-by-vamshi <krishnavamshidevarasetty@gmail.com>
2023-09-26 07:52:29 +0530

    Added Comments

--- Search.py
+++ Search.py
@@ -22,6 +22,7 @@ class Search(ABC):
         return self._items
 
     def _time(self, item):
+        # Returns time taken for searching the item in the list
         self._time = time.perf_counter()
         self._search(item)
         self._time = time.perf_counter() - self._time
@@ -34,9 +35,12 @@ class LinSearch(Search):
     """Class that represents a Linear Search implementation."""
 
     def _search(self, item):
+        # Searches item in the list by iterating over list by one item after the other
         for i in self.get_items():
             if i == item:
+                # Returns True when Item found
                 return True
+        # If item not found returns False
         return False
 
 
@@ -46,18 +50,24 @@ class BinSearch(Search):
     """Class that represents a Binary Search implementation."""
 
     def _search(self, item):
+        # Searches the item in list(which is sorted)
         _items = self.get_items()
         left, right = 0, len(_items)-1
         while True:
             if(left > right):
+                # Breaking the loop when left index becomes greater than right
                 break
             else:
+                # Calculating middle element index in our search space
                 mid = (left + right) //2
                 if(_items[mid] == item):
+                    # Returns True if element Found
                     return True
                 else:
                     if(_items[mid] < item):
+                        # If item is greater than middle element, search space changes to second half of current search space
                         left = mid + 1
                     else:
+                        # If item is less than middle element, search space changes to first half of current search space
                         right = mid - 1
         return False
--- Sort.py
+++ Sort.py
@@ -22,6 +22,7 @@ class Sort(ABC):
         return self._items
 
     def _time(self):
+        # Returns time taken for sorting the list
         self._time = time.perf_counter()
         self._sort()
         self._time = time.perf_counter() - self._time
@@ -34,11 +35,15 @@ class BubbleSort(Sort):
     """Class that represents a BubbleSort implementation."""
 
     def _sort(self):
+        # Making a copy of list to sort
         _items = self.get_items().copy()
+        # Iterating over list, comparing adjacent elements and swapping them if necessary
         for i in range(len(_items)):
             for j in range(len(_items)-i-1):
                 if _items[j] > _items[j+1]:
+                    # Swapping the elements if they are out of order
                     _items[j],_items[j+1] = _items[j+1],_items[j]
+        # Return the sorted list
         return _items
 
 """Module with the implementation of the MergeSort algorithm."""
@@ -47,23 +52,31 @@ class MergeSort(Sort):
     """Class that represents a MergeSort implementation."""
 
     def _sort(self):
+        # Making a copy of list to sort
         _items = self.get_items().copy()
+        # Initializing the size of sub array
         size = 1
         while True:
             if(size >= len(_items)):
                 break
             else:
                 for i in range(0,len(_items),size*2):
+                    # Dividing the array into two sub arrays
                     left = _items[i : i+size]
                     right = _items[i+size : i+size*2]
+                    # Merging the sub arrays
                     merged = self._merge(left,right)
+                    # Updating the main array with merged subarrays
                     _items[i : i+len(merged)] = merged
+                # Doubling the size of subarray for next iteration
                 size = size*2
+        # Return sorted list
         return _items
 
     def _merge(self, left, right):
         merged = []
         i,j = 0,0
+        # Merging two subarrays in sorted order
         while i < len(left) and j < len(right):
             if(left[i] < right[j]):
                 merged.append(left[i])
@@ -71,8 +84,10 @@ class MergeSort(Sort):
             else:
                 merged.append(right[j])
                 j+=1
+        # Adding any remaining elements from the subarray if any  
         if(i!=len(left)):
             merged.extend(left[i:])
         if(j!=len(right)):
             merged.extend(right[j:])
+        # Returning the merged subarray
         return merged
